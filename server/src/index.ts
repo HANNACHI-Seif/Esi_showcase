de357bf0ec8b5755fcda38ce6c353133f9084feb
HANNACHI-Seif <shannachi2@gmail.com>
2022-12-11 17:22:40 +0100

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("kach 3fsa")
+import express, { Request, Response} from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Listening..");
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hehe');
+})
