48d39b86b37523cddf59a5d3dabb4bd9111ec4a5
poysa213 <y.hanaia@esi-sba.dz>
2022-12-13 12:59:27 +0100

    add tailwind css

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-white">
+      <h1 className='text-red-900'>Hih awdi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <h1 className='text-red-900'>Hih awdi</h1>
+  </React.StrictMode>
+);
